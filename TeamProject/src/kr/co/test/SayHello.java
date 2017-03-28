c88f64f53836e768e31cb256d5d36b4d5db358e3
SaYeob-Kim <lietter2@naver.com>
2017-03-28 11:13:02 +0900

    Update SayHello.java

--- TeamProject/src/kr/co/test/SayHello.java
+++ TeamProject/src/kr/co/test/SayHello.java
@@ -3,5 +3,6 @@ package kr.co.test;
 public class SayHello {
 	public static void main(String[] args) {
 		System.out.println("원격저장소테스트를 위한 실습");
+		System.out.println("it is good way to relieve stress");
 	}
 }
