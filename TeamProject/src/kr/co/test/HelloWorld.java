0e21d40134b41d9325e0bac5712d7a7e961eaf03
SaYeob <lietter2@naver.com>
2017-03-28 10:04:13 +0900

    second에서 커밋

--- TeamProject/src/kr/co/test/HelloWorld.java
+++ TeamProject/src/kr/co/test/HelloWorld.java
@@ -3,5 +3,6 @@ package kr.co.test;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello!! haha");
+		System.out.println("second branch에서 작업한내용!!");
 	}
 }
